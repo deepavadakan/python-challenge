024b0c24b5e2f4985efd168b65e508794f216228
Deepa <deepa.vadakan@gmail.com>
2020-09-23 15:30:46 -0400

    completed PyBoss

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -2,7 +2,7 @@ import csv
 import os
 from datetime import datetime
 
-#to find the state abbreviation
+#dict to find the state abbreviation
 us_state_abbrev = {
     'Alabama': 'AL',
     'Alaska': 'AK',
@@ -56,42 +56,58 @@ us_state_abbrev = {
     'Wyoming': 'WY',
 }
 
-#set path to the election_data.csv which is in the resources folder
-#csvpath = os.path.join("Resources","election_data.csv")
 #csv file from which to read data
-hr_data_path = "employee_data.csv"
-#csv file to write the formatted data
-newhr_data_path ="newemployee_data.csv"
+hr_data_path = os.path.join("employee_data.csv")
 
-#in both csv files
-with open(hr_data_path) as hrfileStream, open(newhr_data_path, "w") as newhrfileStream: 
+#open employee_data.csv to read
+with open(hr_data_path) as hrfileStream: 
 
-    #open the files for reading and writing
     hr_datareader = csv.reader(hrfileStream) 
-    newhr_datareader = csv.writer(newhrfileStream) 
     
     #find the header row
     hr_data_header = next(hr_datareader) 
+
+    #create lists for each column
+    emp_ID = []
+    first_name = []
+    last_name = []
+    dob = []
+    ssn = []
+    state = []
     
+    #for each row in the file
+    for row in hr_datareader:
+
+        #add emp_IDs to list
+        emp_ID.append(row[0])
+
+        #find and split the name into first and last name lists
+        emp_name = row[1].split()
+        first_name.append(emp_name[0])
+        last_name.append(emp_name[1])
+
+        #format date to mm/dd/yyyy and add to list
+        date_time_object = datetime.strptime(row[2], '%Y-%m-%d')
+        dob.append(date_time_object.strftime("%m/%d/%Y"))
+
+        #add modified ssn to list
+        new_ssn = "****-**-" + row[3][-4:] #append last 4 digits to masked ssn
+        ssn.append(new_ssn)
+
+        #find the abbreviated State and add to list
+        state.append(us_state_abbrev[row[4]])
+
+#csv file to write the formatted data
+newhr_data_path = os.path.join("newemployee_data.csv")
+
+with open(newhr_data_path, "w") as newhrfileStream:
+
+    newhr_datareader = csv.writer(newhrfileStream) 
+
     #add new header row   
     newhr_dataheader = [hr_data_header[0], "First Name", "Last Name", hr_data_header[2], hr_data_header[3], hr_data_header[4]]
     newhr_datareader.writerow(newhr_dataheader)
 
-    #for each row in the file
-    for row in hr_datareader:
-
-        #find and split the name into first and last name
-        emp_name = row[1].split()
-        first_name = emp_name[0]
-        last_name = emp_name[1]
-
-        #format date to mm/dd/yyyy
-        date_time_object = datetime.strptime(row[2], '%Y-%m-%d')
-        hr_date = date_time_object.strftime("%m/%d/%Y")
-
-        #find the abbreviated State
-        hr_state = us_state_abbrev[row[4]]
-
-        #create the row and add to the new csv file
-        newhr_row = [row[0], first_name, last_name, hr_date, row[3], hr_state]
-        newhr_datareader.writerow(newhr_row)
+    #zip all the lists and add them to the csv file
+    hr_data = zip(emp_ID, first_name, last_name, dob, ssn, state)
+    newhr_datareader.writerows(hr_data)
